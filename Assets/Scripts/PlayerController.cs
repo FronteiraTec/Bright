ca649135971f160e046e2f0faa1e405005ae7a7a
Douglas Kosvoski <douglas.contactpro@gmail.com>
2020-04-08 19:52:19 -0300

    Criado pulo, refatorado player, adicionado cinemachine e maquina de estado finito

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,20 +1,100 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
-    // atribuir referencia do objeto no inspector mode
-    [SerializeField] private StaminaBar staminaBar;
+  // Inspector Variables
+  [SerializeField]private int speed = 5;
+  [SerializeField]private float jumpForce = 20f;
+  [SerializeField]private LayerMask ground;
+  [SerializeField]private StaminaBar staminaBar;
 
+  // Finite State Machine
+  private enum State {idle, running, jumping, falling, hurt};
+  private State state = State.idle;
 
-    private void Update()
+  // Start() Variables
+  private Rigidbody2D rb;
+  private Animator anim;
+  private Collider2D coll;
+
+  private void Start()
+  {
+      rb = GetComponent<Rigidbody2D>();
+      anim = GetComponent<Animator>();
+      coll = GetComponent<Collider2D>();
+  }
+
+  // Update is called once per frame
+  private void Update()
+  {
+    Movement();
+    AnimationState();
+    anim.SetInteger("state",(int)state);
+  }
+
+  private void Jump()
+  {
+    staminaBar.UseStamina(12f);
+    rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    state = State.jumping;
+  }
+
+  private void Movement()
+  {
+    if (Input.GetKeyDown(KeyCode.R))
     {
-        // exemplo de uso da barra de stamina atravez do player
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            // chamada do metodo do uso da stamina
-            staminaBar.UseStamina(12f);
-        }
+      SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
+    float HDirection = Input.GetAxis("Horizontal");
+    // Moving Left
+    if(HDirection < 0)
+    {
+      rb.velocity = new Vector2(-speed, rb.velocity.y);
+      transform.localScale = new Vector2(-1, 1);
+    }
+    // Moving Right
+    else if(HDirection > 0)
+    {
+      rb.velocity = new Vector2(speed, rb.velocity.y);
+      transform.localScale = new Vector2(1, 1);
+    }
+    
+    if(Input.GetButtonDown("Jump") && coll.IsTouchingLayers(ground))
+    {
+      Jump();
+    }
+  }
+  
+  private void AnimationState()
+  {
+    if(state == State.jumping)
+    {
+      if(rb.velocity.y < .1f)
+      {
+        state = State.falling;
+      }
+    }
+    else if(state == State.falling)
+    {
+      if(coll.IsTouchingLayers(ground))
+      {
+        state = State.idle;
+      }
+    }
+
+    else if(Mathf.Abs(rb.velocity.x) > 2f)
+    {
+      // Moving
+      state = State.running;
+    }
+    else
+    {
+      state = State.idle;
+    }
+  }
 }
+
+
